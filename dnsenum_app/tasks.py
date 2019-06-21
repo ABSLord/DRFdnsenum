718e70ffad0ef8877430f27e159295eae3fc72af
Anton <antonio.maksimovich@gmail.com>
2019-06-21 13:10:25 +0300

    Refactoring

--- dnsenum_app/tasks.py
+++ dnsenum_app/tasks.py
@@ -1,7 +1,36 @@
 from DRFTest.celery import app
+import os
+import subprocess
+
+from dnsenum_app.utils import *
 
 
 @app.task
 def hello_task():
-    # TODO: add task here
     print("hello")
+
+
+@app.task
+def call_dns_enum(domain):
+    process = subprocess.Popen(
+        [
+            "perl",
+            os.path.join(os.getcwd(), "dnsenum_app", "dnsenum.pl"),
+            domain,
+            "--nocolor"
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    process.wait()
+    result = parse_dnsenum_output(process.communicate()[0].decode('UTF-8'))
+    if process.returncode:
+        return {
+            'status': 'error',
+            'message': result
+        }
+    else:
+        return {
+            'status': 'success',
+            'data': result
+        }
